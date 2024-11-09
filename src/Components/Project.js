6a9ea9bfdc475f67ef363aca78fda08039f10a38
pratikshakunghadkar2003 <kunghadkar2003@gmail.com>
2024-11-09 18:10:26 +0530

    commit changes

---
+++ src/Components/Project.js
@@ -0,0 +1,180 @@
+import React from 'react'
+
+export default function Project() {
+  return (
+    <div className='container-fluide'>
+      <div className='row'>
+        <div className='col-md-2'>
+          <h2>File-Tracker</h2>
+
+          <div className='section'>
+            <h4>Dashboard</h4>
+            <h4>Track File</h4>
+            <h4>Scan File</h4>
+          </div>
+          <div className='section-one'>
+           
+          <h4>Log Out</h4>
+          </div>
+        </div>
+        <div className='col-md-10'>
+          <div className='dashboard'> <h2>Dashboard &gt;&gt;</h2></div>
+
+          <h5>Hi,Pratiksha</h5>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjed6fqvW3kr0v-gtD6OLycMKFjaJ4A5aCWg&s"></img>
+
+
+          <nav class="navbar navbar-expand-lg">
+              <div class="container-fluid">
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Filter</a>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/">Action</a></li>
+                        <li><a class="dropdown-item" href="/">Another action</a></li>
+                        <li><hr class="dropdown-divider"></hr></li>
+                        <li><a class="dropdown-item" href="/">Something else here</a></li>
+                      </ul>
+                    </li>
+
+                  </ul>
+                  <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                  </form>
+                </div>
+              </div>
+            </nav>
+
+
+          <div className='section-div'>
+           
+
+            <table className="table  table-striped">
+              <thead class="table-success">
+                <tr>
+                  <th scope="col"><input type='checkbox'></input></th>
+                  <th scope="col">Sr.No.</th>
+                  <th scope="col">File Number</th>
+                  <th scope="col">File Name</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Action</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+               
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
